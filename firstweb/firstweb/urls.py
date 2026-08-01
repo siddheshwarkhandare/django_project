3b8ed6cf4f718ad77c3b4470029ee69f3a9c1238
siddheshwarkhandare <235892541+siddheshwarkhandare@users.noreply.github.com>
2026-08-01 23:38:15 +0530

    some changes

--- firstweb/firstweb/urls.py
+++ firstweb/firstweb/urls.py
@@ -18,11 +18,13 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from . import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('web/', include('web.urls')),
+    path('/', view=home,name="index.html"),
 
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
