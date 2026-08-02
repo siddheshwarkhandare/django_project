36ef293d12cce28823bd398ca6ee151b50393778
siddheshwarkhandare <235892541+siddheshwarkhandare@users.noreply.github.com>
2026-08-02 23:52:34 +0530

    model file edit

--- firstweb/web/views.py
+++ firstweb/web/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Web
 from .forms import WebForm
+from django.shortcuts import get_object_or_404
 
 def index(request):
     return render (request, 'index.html')
