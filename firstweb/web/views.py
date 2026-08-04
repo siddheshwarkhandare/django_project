1863621e55f689e4c8b49f29b500a3b1d7df150a
siddheshwarkhandare <235892541+siddheshwarkhandare@users.noreply.github.com>
2026-08-04 23:12:16 +0530

    views

--- firstweb/web/views.py
+++ firstweb/web/views.py
@@ -6,3 +6,13 @@ from django.shortcuts import get_object_or_404
 def index(request):
     return render (request, 'index.html')
 # Create your views here.
+def weeb_list(request):
+    Web.objects.all().order_by("-created_at")
+    return render(request, 'web_list.html',{'web':Web})
+
+def web_created(request):
+    if request.method == 'POST': 
+        pass
+    else:
+        form =WebForm()
+    return render(request, 'webfrom_list.html',{'from':form})
