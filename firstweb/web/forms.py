152b0a6da231a94fdd4c014f63e4d9d026274b04
siddheshwarkhandare <235892541+siddheshwarkhandare@users.noreply.github.com>
2026-08-10 18:54:08 +0530

    add login

--- firstweb/web/forms.py
+++ firstweb/web/forms.py
@@ -1,9 +1,19 @@
 from django import forms
 from .models import Web
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 
 class WebForm(forms.ModelForm):
     
     class Meta:
         model = Web
-        fields = ("text","photo")
+        fields = ["text","photo"]
+
+
+class UserRagistrationform(UserCreationForm):
+    Email=forms.EmailField()
+    class meta:
+        model=User
+        fields=('username', 'eamil', 'password1','password2')
+
--- firstweb/web/views.py
+++ firstweb/web/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from .models import Web
-from .forms import WebForm
+from .forms import WebForm,UserRagistrationform
 from django.shortcuts import get_object_or_404,redirect
+from django.contrib.auth.decorators import login_required
 
 """
 in views we define the fuctionality of forms we creat each function for the for form 
@@ -17,6 +18,7 @@ def web_list(request):
     return render(request, 'web_list.html',{'webs':webs})  # listing all the web in to this web page 
                                                         #"web is passing the data from web that is modle file"
 # in this fuction we are creating an new entry like tweet 
+@login_required
 def web_created(request):
     if request.method == 'POST': 
         form = WebForm(request.POST,request.FILES) # importing from from the model file 
@@ -29,7 +31,7 @@ def web_created(request):
     else:
         form =WebForm()
     return render(request, 'web_form.html',{'form':form}) #we will seeing the form because of this
-
+@login_required
 def web_edit(request,web_id):
     web= get_object_or_404(Web,pk=web_id,user=request.user)
     if request.method == 'POST':
@@ -42,10 +44,15 @@ def web_edit(request,web_id):
     else:
         form= WebForm(instance= web)# keeping the data prefill
     return render(request,'web_form.html', {'form':form})
-
+@login_required
 def web_delete(request, web_id):
     web=get_object_or_404(Web,pk=web_id, user=request.user)
     if request.method == 'POST':
         web.delete()
         return redirect('web_list')
     return render(request,'web_conf_delete.html', {'web':web})
+
+def regitration(request):
+    return render(request, 'registration/ragister.html',{'form':form})
+
+from .forms import WebForm,Userloginform
